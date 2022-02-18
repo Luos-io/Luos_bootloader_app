097641ff0a6576510ae435a8bb1f627fb7c3d66c
nicolas cros <nicolas.cros@luos.io>
2022-02-18 10:10:43 +0100

    update memory configuration and interrupt handler

--- luos_bootloader_application_G474RE/src/stm32g4xx_it.c
+++ luos_bootloader_application_G474RE/src/stm32g4xx_it.c
@@ -203,27 +203,12 @@ void SysTick_Handler(void)
 /* USER CODE BEGIN 1 */
 void EXTI9_5_IRQHandler(void)
 {
-  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
-
-  /* USER CODE END EXTI9_5_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_8);
-  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
-
-  /* USER CODE END EXTI9_5_IRQn 1 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
 }
 
-/**
- * @brief This function handles EXTI line[15:10] interrupts.
- */
-void EXTI15_10_IRQHandler(void)
+void EXTI4_IRQHandler(void)
 {
-  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
-
-  /* USER CODE END EXTI15_10_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
-  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
-
-  /* USER CODE END EXTI15_10_IRQn 1 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
 }
 /* USER CODE END 1 */
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
